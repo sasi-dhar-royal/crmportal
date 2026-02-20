6bb09b7efc36dd7542c88b1f29271f57ef0a1167
frontend-fuel <neverfail2026@gmail.com>
2026-02-20 14:28:21 +0530

    Fix CORS: remove withCredentials

--- src/utils/axiosConfig.js
+++ src/utils/axiosConfig.js
@@ -5,7 +5,6 @@ const BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:5000';
 
 const api = axios.create({
     baseURL: BASE_URL,
-    withCredentials: true, // Important for cookies/sessions if used
     headers: {
         'Content-Type': 'application/json',
     },
